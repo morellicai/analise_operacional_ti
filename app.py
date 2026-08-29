b04849a6db794447be3be520903c311200c2ba80
Caio Morelli <caiomorelli.it@gmail.com>
2026-08-29 20:29:44 -0300

    Adição de algumas novas visualizaçõe

--- app.py
+++ app.py
@@ -165,28 +165,39 @@ st.title("💻 Painel de Operações TI")
 # Filtramos apenas o nome da tarefa e o ID da lista para inspecionar
 df = df_join[['idShort','Nome do Card', 'Prioridade', 'Data Inicio', 'Data Prevista Entrega', 'Card Finalizado', 'Última atividade', 'Categoria']]
 
-col1, col2, col3= st.columns(3, border=True, gap='large')
+col1, col2, col3, col4 = st.columns(4, border=True, gap='small')
 
 cards_finalizados_count = df['Card Finalizado'].value_counts().get(True, 0)
 count_cards_em_andamento = df_join['Categoria'].value_counts().get('Em andamento', 0)
 cards_finalizados_count_by_priority = df[df['Card Finalizado'] & df['Prioridade'].isin(['Alta', 'Crítico / Urgente'])].shape[0]
 
 with col1:
-    st.metric(label='Volume de Cards Finalizados', value=cards_finalizados_count)
+    st.metric(
+        value=cards_finalizados_count,
+        label='Volume de Cards Finalizados' 
+    )
 with col2:
     st.metric(label='Cards finalizados por prioridade Alta e Crítico / Urgente', value=cards_finalizados_count_by_priority)
 with col3:
     st.metric(label='Volume de Cards em Andamento', value=count_cards_em_andamento)
+with col4:
+    st.metric(label='Total de Cards', value=len(df))
 
 st.space()
-st.subheader("Gráfico de Chamados:")
 
-chart_col1, chart_col2, chart_col3 = st.columns(3, border=True, gap='medium')
+chart_col1, chart_col2 = st.columns(2, border=True, gap='small')
+
+with chart_col1:
+    st.markdown("### 📊 Chamados por Categoria")
+    st.space()
+    st.bar_chart(df_join['Categoria'].value_counts(), use_container_width=True, height=300, width=400)
 
 with chart_col2:
     st.markdown("### 📊 Chamados por Prioridade")
     st.space()
-    st.bar_chart(data_label, x='Prioridade', y='Uso', stack=False)
+    st.bar_chart(data_label, x='Prioridade', y='Uso', stack=False, use_container_width=True, horizontal=True, height=300, width=400)
+
+chart2_col1, chart2_col2 = st.columns(2, border=True, gap='small')
 
 st.subheader("Chamados Brutos:")
 st.dataframe(df, hide_index=True)
