b770a9aeb3a162f7ddcc271a77de4acb52d63887
Caio Morelli <caiomorelli.it@gmail.com>
2026-09-08 21:17:41 -0300

    Adição de filtro por datas

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # http://docs.python-requests.org
 import pandas as pd
 import streamlit as st
+from datetime import date, timedelta
 from api import data_cards, data_label, data_lists
 from cleam_data import cleam_data_cards, cleam_data_labels, cleam_data_lists
 from join import join_tables
@@ -37,14 +38,35 @@ df = df_join[[
 ]]
 
 # Indicadores KPI's
-cards_finalizados_count = df['Card Finalizado'].value_counts().get(True, 0)
+
+df['Data Inicio'] = pd.to_datetime(df['Data Inicio'], errors='coerce', dayfirst=True)
+
+with st.sidebar:
+    st.title("💻 Painel de Operações TI")
+
+    select_date = st.date_input(
+        "Selecione o período de análise", 
+        value=date(2026, 9, 1),
+        format="DD/MM/YYYY"
+    )
+
+if select_date:
+    inicio_periodo = pd.to_datetime(select_date)
+    fim_periodo = inicio_periodo + timedelta(days=7)
+
+    fim_periodo = pd.to_datetime(fim_periodo)
+
+    filtro_periodo = (df['Data Inicio'] >= inicio_periodo) & (df['Data Inicio'] <= fim_periodo)
+    df_filtered = df.loc[filtro_periodo]
+
+cards_finalizados_count = df_filtered['Card Finalizado'].value_counts().get(True, 0)
 count_cards_em_andamento = df_join['Categoria'].value_counts().get('Em andamento', 0)
-cards_finalizados_count_by_priority = df[df['Card Finalizado'] & df['Prioridade'].isin(['Alta', 'Crítico / Urgente'])].shape[0]
+cards_finalizados_count_by_priority = df_filtered[df_filtered['Card Finalizado'] & df_filtered['Prioridade'].isin(['Alta', 'Crítico / Urgente'])].shape[0]
 
 # 3. Configuração da pagina de Exibição no Streamlit
+
 st.set_page_config(page_title="Painel de Operações TI", page_icon="💻", layout="wide")
 
-st.title("💻 Painel de Operações TI")
 
 col1, col2, col3, col4 = st.columns(4, border=True, gap='small')
 
@@ -58,7 +80,7 @@ with col2:
 with col3:
     st.metric(label='Volume de Cards em Andamento', value=count_cards_em_andamento)
 with col4:
-    st.metric(label='Total de Cards', value=len(df))
+    st.metric(label='Total de Cards A Fazer', value=df_filtered['Categoria'].isin(['A Fazer']).sum())
 
 st.space()
 
@@ -67,7 +89,7 @@ chart_col1, chart_col2 = st.columns(2, border=True, gap='small')
 with chart_col1:
     st.markdown("### 📊 Chamados por Categoria")
     st.space()
-    st.bar_chart(df_join['Categoria'].value_counts(), use_container_width=True, height=300, width=400)
+    st.bar_chart(df['Categoria'].value_counts(), use_container_width=True, height=300, width=400)
 
 with chart_col2:
     st.markdown("### 📊 Chamados por Prioridade")
@@ -77,13 +99,15 @@ with chart_col2:
 chart2_col1, chart2_col2 = st.columns(2, border=True, gap='small')
 
 with chart2_col1:
-    st.line_chart(df_join, x='Data Inicio', y='idShort', use_container_width=True, height=300, width=400)
+    st.line_chart(df_filtered, x='Data Inicio', y='idShort', use_container_width=True, height=300, width=400)
 
 st.markdown('---')
 
-st.subheader("Tabela Chamados Brutos:")
+st.subheader("Tabela Chamados:")
+df_filtered['Data Inicio'] = normalize_date_format(df_filtered['Data Inicio'])
+
 select = st.dataframe(
-    df, 
+    df_filtered, 
     hide_index=True,
     on_select="rerun",
     selection_mode="single-row"
@@ -93,6 +117,6 @@ select_line = select["selection"]["rows"]
 
 if select_line:
     index_line = select_line[0]
-    data_line = df.iloc[index_line]
+    data_line = df_filtered.iloc[index_line]
 
     modal_detalhs(data_line)
